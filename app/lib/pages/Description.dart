0cb1ebbd384ecf0f33af09b2c42a34ca5242cf9f
buterajacques1 <jbutera702@gmail.com>
2023-12-05 13:06:54 +0200

    changes

--- app/lib/pages/Description.dart
+++ app/lib/pages/Description.dart
@@ -21,9 +21,27 @@ class DescriptionPage extends StatefulWidget {
 }
 
 class _DescriptionPageState extends State<DescriptionPage> {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('hotels').snapshots();
+  final Stream<QuerySnapshot> _usersStream =
+      FirebaseFirestore.instance.collection('hotels').snapshots();
   late final DatabaseManager _db;
   late List<LocationItem> locationItems = [];
+  
+  // Create a CollectionReference called users that references the firestore collection
+  CollectionReference hotelsRef =
+      FirebaseFirestore.instance.collection('hotels');
+
+  //add hotel
+
+  Future<void> addHotels() {
+    // Call the user's CollectionReference to add a new user
+    return hotelsRef
+        .add({
+          'name': 'testings', // John Doe
+          'Cost': 600, // Stokes and Sons
+        })
+        .then((value) => print("hotel Added"))
+        .catchError((error) => print("Failed to add hotel: $error"));
+  }
 
   @override
   void initState() {
@@ -63,70 +81,123 @@ class _DescriptionPageState extends State<DescriptionPage> {
     );
   }
 
+  //read hotels
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-       if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
+        stream: _usersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          // print(snapshot.data()!.docs);
+          return Scaffold(
+            appBar: AppBar(
+              title:
+                  Text('Locations - ${widget.category} in ${widget.district}'),
+              actions: [
+                IconButton(
+                  icon: Icon(Icons.map),
+                  onPressed: _viewOnMapButtonPressed,
+                ),
+              ],
+            ),
+            body: Container(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: 350.0,
+                    child: ListView(
+                      children:
+                          snapshot.data!.docs.map((DocumentSnapshot document) {
+                        Map<String, dynamic> data =
+                            document.data()! as Map<String, dynamic>;
+                        print(data);
+                        final myController = TextEditingController();
 
-        }
-        // print(snapshot.data()!.docs);
-        return Scaffold(
-      appBar: AppBar(
-        title: Text('Locations - ${widget.category} in ${widget.district}'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.map),
-            onPressed: _viewOnMapButtonPressed,
-          ),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-      
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [ 
-            SizedBox(height: 200.0, child: ListView(
-          children: snapshot.data!.docs.map((DocumentSnapshot document) {
-          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-          print(data);
-            return ListTile(
-              title: Text(data['name']),
-              subtitle: Text(data['Cost'].toString()),
-            );
-          }).toList(),
-        ),)
-             ,
-            const SizedBox(height: 16.0),
-            Text(
-              widget.category,
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
+                        //delete hotel
+                        Future<void> deleteHotel() {
+                          return hotelsRef
+                              .doc(document.id)
+                              .delete()
+                              .then((value) => print("Hotel Deleted"))
+                              .catchError((error) =>
+                                  print("Failed to delete user: $error"));
+                        }
+
+                        Future<void> updateHotels() {
+                          return hotelsRef
+                              .doc(document.id)
+                              .update({'name': myController.text})
+                              .then((value) => print("Hotel Updated"))
+                              .catchError((error) =>
+                                  print("Failed to update hotel: $error"));
+                        }
+
+                        return Column(children: [
+                          GestureDetector(
+                            onTap: () {
+                              deleteHotel();
+                            },
+                            child: Text(data['name']),
+                          ),
+                          Text(data['Cost'].toString()),
+                          TextField(
+                            controller: myController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Update Hotel',
+                            ),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                print(myController.value);
+                                updateHotels();
+                              },
+                              child: Text('Update')),
+                          SizedBox(height: 20.0)
+                        ]);
+                        
+                      }).toList(),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      addHotels();
+                    },
+                    child: Text(
+                      "Add Hotel",
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Text(
+                    widget.category,
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 16.0),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: locationItems.length,
+                      itemBuilder: (context, index) {
+                        return LocationItemCard(item: locationItems[index]);
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 16.0),
-            Expanded(
-              child: ListView.builder(
-                itemCount: locationItems.length,
-                itemBuilder: (context, index) {
-                  return LocationItemCard(item: locationItems[index]);
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-    });
-     
+          );
+        });
   }
 }
 
@@ -151,7 +222,8 @@ class LocationItemCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(6.0),
               image: DecorationImage(
-                image: AssetImage('assets/location_image.jpg'), // Change this based on your images
+                image: AssetImage(
+                    'assets/location_image.jpg'), // Change this based on your images
                 fit: BoxFit.cover,
               ),
             ),
